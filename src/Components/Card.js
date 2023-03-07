af00e2a60b3dc86668f5ec141583e2846df22fc9
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-03-07 21:07:12 +0900

    Add basic logics to Card.js and Cards.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,5 +1,10 @@
-const Card = () => {
-  return <div></div>;
+const Card = ({ obj }) => {
+  return (
+    <div>
+      <img src={obj.imgPath} alt={obj.text} width="100" height="100" />
+      <p>{obj.text}</p>
+    </div>
+  );
 };
 
 export default Card;
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import Card from "./Card";
+
 const Cards = () => {
-  return <div></div>;
+  const [allCardsArr, setAllCardsArr] = useState([
+    { id: 1, text: "Description 1", imgPath: "../images/1.png" },
+    { id: 2, text: "Description 2", imgPath: "../images/2.png" },
+    { id: 3, text: "Description 3", imgPath: "../images/3.png" },
+    { id: 4, text: "Description 4", imgPath: "../images/4.png" },
+    { id: 5, text: "Description 5", imgPath: "../images/5.png" },
+    { id: 6, text: "Description 6", imgPath: "../images/6.png" },
+    { id: 7, text: "Description 7", imgPath: "../images/7.png" },
+    { id: 8, text: "Description 8", imgPath: "../images/8.png" },
+  ]);
+  return (
+    <div>
+      {allCardsArr.map((obj) => (
+        <Card obj={obj} />
+      ))}
+    </div>
+  );
 };
 
 export default Cards;
