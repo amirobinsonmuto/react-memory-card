954a65994103113711125c2c3de2bb80649d7cb4
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-03-09 10:28:19 +0900

    Add currentScore state and calculateCurrentScore method

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Cards from "./Components/Cards";
+import Score from "./Components/Score";
 
 function App() {
-  const [clickedCardIdArr, setClickedCardIdArr] = useState([1, 2]);
+  const [clickedCardIdArr, setClickedCardIdArr] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
 
   const addToClickedCardIdArr = (clickedCardObjId) => {
     setClickedCardIdArr([...clickedCardIdArr, clickedCardObjId]);
   };
 
+  const calculateCurrentScore = () => {
+    setCurrentScore(clickedCardIdArr.length);
+  };
+
+  // calculate current score when there is a change in the clickedCardIdArr state
+  useEffect(() => {
+    calculateCurrentScore();
+  }, [clickedCardIdArr]);
+
   return (
     <div className="App">
+      <Score currentScore={currentScore} />
       <Cards addToClickedCardIdArr={addToClickedCardIdArr} />
     </div>
   );
--- src/Components/Score.js
+++ src/Components/Score.js
@@ -1,5 +1,15 @@
-const Score = () => {
-  return <div></div>;
+import PropTypes from "prop-types";
+
+const Score = ({ currentScore }) => {
+  return (
+    <div>
+      <p>Current Score: {currentScore}</p>
+    </div>
+  );
+};
+
+Score.propTypes = {
+  currentScore: PropTypes.number.isRequired,
 };
 
 export default Score;
