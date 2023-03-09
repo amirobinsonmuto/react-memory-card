520330659dd6493b195d48cf801e2bc2f2487c2d
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-03-09 09:54:41 +0900

    Add shuffleCards prop and proptypes validation

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,10 +1,17 @@
-const Card = ({ obj }) => {
+import PropTypes from "prop-types";
+
+const Card = ({ obj, shuffleCards }) => {
   return (
-    <div>
+    <div onClick={shuffleCards}>
       <img src={obj.imgPath} alt={obj.text} width="100" height="100" />
       <p>{obj.text}</p>
     </div>
   );
+};
+
+Card.propTypes = {
+  obj: PropTypes.object.isRequired,
+  shuffleCards: PropTypes.func.isRequired,
 };
 
 export default Card;
