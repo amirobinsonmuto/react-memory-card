c4e625c5ea410e289ea6b8300df7e06cf284180d
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-03-10 18:18:20 +0900

    Update image paths

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -4,18 +4,18 @@ import Card from "./Card";
 
 const Cards = ({ addToClickedCardIdArr }) => {
   const [cardArr, setCardArr] = useState([
-    { id: 1, text: "Shiba Inu", imgPath: "../images/1.png" },
-    { id: 2, text: "Golden Retriever", imgPath: "../images/2.png" },
-    { id: 3, text: "Doberman", imgPath: "../images/3.png" },
-    { id: 4, text: "Beagle", imgPath: "../images/4.png" },
-    { id: 5, text: "American Eskimo", imgPath: "../images/5.png" },
-    { id: 6, text: "Miniature Pinscher", imgPath: "../images/6.png" },
-    { id: 7, text: "Labrador", imgPath: "../images/7.png" },
-    { id: 8, text: "Scottish Terrier", imgPath: "../images/8.png" },
-    { id: 9, text: "French Bulldog", imgPath: "../images/9.png" },
-    { id: 10, text: "Bull Terrier", imgPath: "../images/10.png" },
-    { id: 11, text: "Dalmatian", imgPath: "../images/11.png" },
-    { id: 12, text: "Boston Terrier", imgPath: "../images/12.png" },
+    { id: 1, text: "Shiba Inu", imgPath: "/images/1.png" },
+    { id: 2, text: "Golden Retriever", imgPath: "/images/2.png" },
+    { id: 3, text: "Doberman", imgPath: "/images/3.png" },
+    { id: 4, text: "Beagle", imgPath: "/images/4.png" },
+    { id: 5, text: "American Eskimo", imgPath: "/images/5.png" },
+    { id: 6, text: "Miniature Pinscher", imgPath: "/images/6.png" },
+    { id: 7, text: "Labrador", imgPath: "/images/7.png" },
+    { id: 8, text: "Scottish Terrier", imgPath: "/images/8.png" },
+    { id: 9, text: "French Bulldog", imgPath: "/images/9.png" },
+    { id: 10, text: "Bull Terrier", imgPath: "/images/10.png" },
+    { id: 11, text: "Dalmatian", imgPath: "/images/11.png" },
+    { id: 12, text: "Boston Terrier", imgPath: "/images/12.png" },
   ]);
 
   const shuffleCards = () => {
